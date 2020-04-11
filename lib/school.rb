60e01cb98312e7fbca34b8d157561437ee814d4f
Vladimir J <vladdaimpaler@protonmail.com>
2020-04-11 20:55:14 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+  attr_accessor :schoolname
+  attr_reader :roster
+  
+  #key = grade, value = ["Student", "List", "In", "Array", "Form"]  
+  
+  def initialize(_schoolName)
+    @schoolname = _schoolName
+    @roster = {}
+  end
+  
+  def add_student(studentName, studentGrade)
+    if @roster.include?(studentGrade) 
+      @roster[studentGrade] << studentName
+    else
+      @roster[studentGrade] = []
+      @roster[studentGrade] << studentName
+    end
+  end
+  
+  def grade(gradeLevel)
+    return @roster[gradeLevel] unless !@roster.include?(gradeLevel)
+  end
+  
+  def sort
+    @roster.sort
+  end
+
+end
